3c3edbee63881d139bc44afafb6d67ca30e51b8b
Lewis Burgess <lewis.burgess@outlook.com>
2024-06-26 11:08:32 -0800

    start of exercises 2.1-2.5

--- test/testApp/src/App.jsx
+++ test/testApp/src/App.jsx
@@ -1,59 +1,15 @@
-import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-  
-  const setZero = () => {
-    setLeft(0);
-    setRight(0);
-    setAll([]);
-    setTotal(0);
-  }
-
-  const History = (props) => {
-    if (props.allClicks.length === 0) {
-      return (
-        <div>
-          the app is used by pressing the buttons
-        </div>
-      )
-    }
-    return (
-      <div>
-        button press history: {props.allClicks.join(' ')}
-      </div>
-    )
-  }
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    console.log('left before', left)
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft)
-    console.log('left after', updatedLeft)
-    setTotal(updatedLeft + right) 
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
+const App = (props) => {
+  const { notes } = props
 
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
-      <div>
-        <button onClick={setZero}>Reset</button>
-      </div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => 
+        <Note key={note.id} note={note}/>)}
+      </ul>
     </div>
   )
 }
