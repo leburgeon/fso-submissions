3f8e5953c29a8daa208377bc066293c60450cdf3
Lewis Burgess <lewis.burgess@outlook.com>
2024-08-05 13:16:40 -0800

    Adding a new blog requires a valid token

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -2,6 +2,8 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const { idValidationMiddlewear } = require('../utils/middlewear')
+const config = require('../utils/config')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.use('/:id', idValidationMiddlewear)
 
@@ -10,19 +12,52 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-  // Temp getting a valid user id to add to the blog
-  const validUser = await User.findOne({})
-  // Creates a mongoose document from the request body
-  const blog = new Blog({...request.body, user: validUser._id})
-  // Awaits for the returned added document from mongo
+const getTokenFrom = (request) => {
+  // Retrieves the value associated with 'authorization' header
+  const authorization = request.get('authorization')
+
+  // If the string starts with 'Bearer' (the scheme for authenticating)
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
+
+blogsRouter.post('/', async (req, res, next) => {
+  // Retrieves the token from the request body
+  const authToken = getTokenFrom(req)
+
+  if (!authToken){
+    return res.status(401).json({error: 'must provide token'})
+  }
+
+  // Verifies the token using the env SECRET
+  const decoded = jwt.verify(authToken, config.SECRET)
+
+  // If the token is invalid, returns status and error
+  if (!(decoded && decoded.id)){
+    return res.status(401).json({error: 'token invalid'})
+  }
+
+  // Finds the user using the user id in the payload
+  const user = await User.findById(decoded.id)
+
+  if (!user){
+    return res.status(401).json({error: 'user not found'})
+  }
+
+  // Adds the new blog to the server using the request body and the found users id
+  const blog = new Blog({...req.body, user: user._id})
   const addedBlog = await blog.save()
-  // Appends the idObject of the blog to the array of ids on the user
-  validUser.blogs.push(addedBlog._id)
-  await validUser.save()
-  // Sets the response status and sends the response as a json
-  response.status(201).json(addedBlog)
+
+  // Adds the id of the new blog to the array of blog ids on the user document and saves the user
+  user.blogs.push(addedBlog._id)
+  await user.save()
+
+  res.status(201).json(addedBlog)
 })
+
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
