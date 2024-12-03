730b6581b2d9fe391ad0979605c01d5c7dc8abeb
Lewis Burgess <lewis.burgess@outlook.com>
2024-12-03 12:15:40 +0000

    Examples

--- part9/typescript-examp/calculator.ts
+++ part9/typescript-examp/calculator.ts
@@ -6,23 +6,25 @@ const calculator = (a: number, b: number, op: Operation) : number => {
     case 'multiply':
       return a * b;
     case 'divide':
-
       if (b === 0) throw new Error('Can\'t divide by 0!');
       return a / b;
     case 'add':
       return a + b;
     default:
-
       throw new Error('Operation is not multiply, add or divide!');
   }
 }
 
-try {
-  console.log(calculator(1, 5 , 'divide'));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message;
+export default calculator
+
+if (require.main === module){
+  try {
+    console.log(calculator(1, 5 , 'divide'));
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong: '
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- part9/typescript-examp/index.ts
+++ part9/typescript-examp/index.ts
@@ -1,13 +1,41 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
+import calculator from './calculator';
+import { Operation } from './calculator';
+
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
-const PORT = 3000
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if ( !value1 || isNaN(Number(value1)) ) {
+    res.status(400).send({ error: 'value 1 was not a number'});
+    return;
+  }
+
+  if ( !value2 || isNaN(Number(value2))) {
+    res.status(400).send({ error: 'value 2 was not a number'});
+    return;
+  }
+
+  // Type assertion only possible when asserting to a more or less specific type. 
+  // Eg booleanVal : true | false = false
+  // const falseBool = booleanVal as false
+
+  // type assertion can be done when the argument is passed to the function
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  res.send({result});
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
